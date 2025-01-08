d318f6b9a9529ff71123b0d2381ef2e4df1519c5
Ramandeep <deepramanrao@gmail.com>
2025-01-08 17:16:51 +0530

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SeacrchInput from "./SeacrchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SeacrchInput />
+      <SeacrchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
